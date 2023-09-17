fff4f0f29d64a9b2e4d4b53900ed208c1baf8cc0
Jonatan Waldow <jonatanwaldow@outlook.com>
2023-09-16 22:53:27 -0300

    Criação do código que sabe validar um e-mail no C#.

---
+++ CS/ValidacaoEmail/ValidacaoEmail/ValidadorDeEmail.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ValidacaoEmail
+{
+    public class ValidadorDeEmail
+    {
+        public static bool EmailValido(string email)
+        {
+            if ((email == null) || email.Length < 4)
+                return false;
+            
+            var validador = new Regex("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
+            return validador.Match(email).Success;
+        }
+    }
+}
---
+++ CS/ValidacaoEmail/ValidacaoEmailTest/ValidadorDeEmailsTest.cs
@@ -0,0 +1,31 @@
+using ValidacaoEmail;
+
+namespace ValidacaoEmailTest
+{
+    [TestClass]
+    public class ValidadorDeEmailsTest
+    {
+        [TestMethod]
+        public void ValidarEmailsDeTeste()
+        {
+            // Retorna Válidos
+            ValidadorDeEmail.EmailValido("1@1.com").Should().BeTrue();
+            ValidadorDeEmail.EmailValido("jose123@outlook.com").Should().BeTrue();
+            ValidadorDeEmail.EmailValido("jose123@pm.me").Should().BeTrue();
+            ValidadorDeEmail.EmailValido("j@j.com").Should().BeTrue();
+            ValidadorDeEmail.EmailValido("joaovs@gmail.com").Should().BeTrue();
+            ValidadorDeEmail.EmailValido("jon@sc.gov.br").Should().BeTrue();
+            ValidadorDeEmail.EmailValido("jon@x.com").Should().BeTrue();
+
+            // Retorna Inválido
+            ValidadorDeEmail.EmailValido("j@j").Should().BeFalse();
+            ValidadorDeEmail.EmailValido("@gmail.com").Should().BeFalse();
+            ValidadorDeEmail.EmailValido("joaovs@.com").Should().BeFalse();
+            ValidadorDeEmail.EmailValido("joaovs@@.com").Should().BeFalse();
+            ValidadorDeEmail.EmailValido("joaovs*gmail.com").Should().BeFalse();
+            ValidadorDeEmail.EmailValido("joaovs-gmail.com").Should().BeFalse();
+            ValidadorDeEmail.EmailValido("joaovs.gmail.com").Should().BeFalse();
+            ValidadorDeEmail.EmailValido("https://solucao.dev").Should().BeFalse();
+        }
+    }
+}
